d8f5f10a774f3b08a9c63ede0f0c24a56e85dc07
iadokonat <iadokonat0106@gmail.com>
2022-10-28 08:32:00 +0900

    implement signup function

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,6 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.shortcuts import render
 
 # Create your views here.
 
+
 def signup(request):
-    return render(request, 'signup.html', {})
+    # object = User.objects.get(username="tanaka")
+    # print(object.password)
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.create_user(username, "", password)
+            return render(request, 'signup.html', {})
+        except IntegrityError:
+            return render(request, "signup.html", {"error": "すでに登録されています"})
+    # else:
+    #     print("this is not post method")
+
+    return render(request, "signup.html", {})
